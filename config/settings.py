030e45e914eb5aa87aa934057cdc075226dfe9c0
Aidyn <138883909+Aidyn010709@users.noreply.github.com>
2024-07-19 18:58:40 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -154,7 +154,12 @@ AUTH_USER_MODEL = 'account.CustomUser'
 
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:3003',
+    'http://localhost:3004',
+    'http://localhost:3005'
 ]
 
 
